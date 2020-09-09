abd5cf0f26b53bbd0b72478386f38b554bc2f2a0
strubloid <joserafaelmb@gmail.com>
2020-09-09 18:44:40 +0100

    finishing basic structure for user list and place

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import Users from './user/pages/Users'
+import NewPlace from './places/pages/NewPlace'
 
-function App() {
-  return <h1>Let's start!</h1>;
+class App extends Component {
+  state = {}
+
+  render () {
+    return <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Users />
+          </Route>
+          <Route exact path="/places/new">
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+    </>
+  }
 }
 
 export default App;
