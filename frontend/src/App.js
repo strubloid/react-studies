11517dbe9404b4756c5199fc4ea47a17edca0073
strubloid <joserafaelmb@gmail.com>
2020-09-09 17:54:54 +0100

    starting a new project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,7 @@
-import React, { Component } from 'react';
-import GoalList from "./components/Goallist/GoalList"
-import NewGoal from "./components/Goallist/New/NewGoal"
+import React from 'react';
 
-import './App.css';
-
-class App extends Component {
-
-    state = {
-        courseGoals: [{}]
-    }
-
-    constructor (props) {
-        super(props);
-        this.state.courseGoals = [
-            {id: 'cg1', text: 'Finish the course'},
-            {id: 'cg2', text: 'Learn all about the course Main topic'},
-            {id: 'cg3', text: 'Help other students in the course Q&A'}
-        ]
-    }
-
-    addNewGoalHandler = newGoal => {
-        this.setState(state => ({courseGoals: [...state.courseGoals, newGoal]}));
-    }
-
-    render () {
-        return (
-            <React.Fragment>
-                <div className="course-goals">
-                    <h2>Course Goals</h2>
-                    <NewGoal onAddGoal={this.addNewGoalHandler} />
-                    <GoalList goals={this.state.courseGoals} />
-                </div>
-            </React.Fragment>
-        );
-    }
+function App() {
+  return <h1>Let's start!</h1>;
 }
 
 export default App;
