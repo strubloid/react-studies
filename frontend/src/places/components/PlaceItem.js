77ec04a449531d33a4e3a8f10fb2122370208c54
strubloid <joserafaelmb@gmail.com>
2020-09-10 17:14:23 +0100

    adding the funtionality of the map

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -2,9 +2,10 @@ import React , {useState} from 'react';
 
 import Card from '../../shared/components/UIElements/Card'
 import Modal from '../../shared/components/UIElements/Modal'
+import Button from '../../shared/components/FormElements/Button'
+import Map from '../../shared/components/UIElements/Map'
 
 import './PlaceItem.css'
-import Button from '../../shared/components/FormElements/Button'
 
 const PLaceItem = (props) => {
 
@@ -23,7 +24,7 @@ const PLaceItem = (props) => {
                 footer={<Button onClick={closeMapHandler}>Close</Button>}
             >
                 <div className="map-container">
-                    <h2>The Map</h2>
+                    <Map center={props.coordinates} zoom={16}/>
                 </div>
             </Modal>
             <li className="place-item">
