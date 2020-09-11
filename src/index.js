05ee7f8be60d161364681377cab22d088371c93d
strubloid <joserafaelmb@gmail.com>
2020-09-11 16:39:11 +0100

    trying to remove the dotenv on live as dont have it at all

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-require('dotenv').config()
-// if(process.env.NODE_ENV !== 'production'){
-// }
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
